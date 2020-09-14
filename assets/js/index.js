1d24ae7fac741f4c54065c2e0dce378ee45e7ed1
spring <842437627@qq.com>
2020-09-14 20:22:44 +0800

    后台主页--实现退出功能,并做了3处小优化

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,10 +19,12 @@ $(function () {
             }
         })
     }
+
     // 封装渲染用户数据的函数,把登录用户的数据渲染到页面
     function renderAvatar(user) {
         //   判断一下用户有没有头像
         var name = user.username || user.nickname
+        $("#welcome").html("欢迎&nbsp&nbsp" + name)
         // 判断用户有没有头像信息
         if (user.user_pic == "") {
             // 如果没有头像则拿取用户昵称的首个字母为头像
@@ -34,8 +36,25 @@ $(function () {
 
             $(".text-avatar").hide()
             // 获取用户头像,并且把用户的头像设为头像
-            $("#userhead").att("src", user.user_pic).show()
+            $("#userhead").attr("src", user.user_pic).show()
 
         }
     }
+
+    // // 右上角退出按钮,实现退出功能
+    // 1.清除主页用户token
+    // // 2.跳转登录页面
+    var layer = layui.layer
+    $("#icon-tuichu").on("click", function (e) {
+        console.log(111)
+        e.preventDefault()
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            // 1. 清空本地存储中的 token
+            localStorage.removeItem('token')
+            // 2. 重新跳转到登录页面
+            location.href = '/login.html'
+
+        })
+    })
 })
