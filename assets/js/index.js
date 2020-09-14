5ae803d4a7935b627ffc794d4d44bd93c9f5bca0
spring <842437627@qq.com>
2020-09-14 19:35:27 +0800

    后台主页--渲染用户头像

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,7 +15,27 @@ $(function () {
                     return "获取用户基本信息失败"
                 }
                 console.log(res)
+                renderAvatar(res.data)
             }
         })
+    }
+    // 封装渲染用户数据的函数,把登录用户的数据渲染到页面
+    function renderAvatar(user) {
+        //   判断一下用户有没有头像
+        var name = user.username || user.nickname
+        // 判断用户有没有头像信息
+        if (user.user_pic == "") {
+            // 如果没有头像则拿取用户昵称的首个字母为头像
+            var first = name[0].toUpperCase()
+            $(".text-avatar").show().html(first)
+            $("#userhead").hide()
+
+        } else {
+
+            $(".text-avatar").hide()
+            // 获取用户头像,并且把用户的头像设为头像
+            $("#userhead").att("src", user.user_pic).show()
+
+        }
     }
 })
