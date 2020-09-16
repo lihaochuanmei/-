44b67687760903a882b0c37c9a1396b3c4d6c510
spring <842437627@qq.com>
2020-09-16 15:23:23 +0800

    文章管理--初始化了发布文章的UI结构

--- assets/js/artisort.js
+++ assets/js/artisort.js
@@ -1,1 +1,14 @@
 // 写一个接口
+$(function () {
+    getArtisort()
+    //  封装一个获取数据的函数
+    function getArtisort() {
+        $.ajax({
+            method: "GET",
+            url: "/my/article/cates",
+            success: function (res) {
+                console.log(res)
+            }
+        })
+    }
+})
